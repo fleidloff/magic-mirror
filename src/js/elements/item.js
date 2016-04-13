6c6fa6f971c34bffcaf5ecc890493c821eca1865
Frederik Leidloff <kontakt@frederik-leidloff.de>
2016-04-13 22:25:00 +0200

    use innerHTML for list

--- src/js/elements/item.js
+++
@@ -1,5 +0,0 @@
-import register, { getSignal } from "../stupidComponent.js";
-
-register("s-item", ({ item }) => {
-  return `<div>${item}</div>`;
-});
--- src/js/elements/list.js
+++ src/js/elements/list.js
@@ -1,7 +1,8 @@
 import register, { getSignal } from "../stupidComponent.js";
 
-register("s-list", ({ items, render, item }) => {
-  if (typeof item === "string") {
+register("s-list", ({ items, render, item, innerHTML }) => {
+  const element = innerHTML || item;
+  if (typeof element === "string") {
     try {
       items = JSON.parse(items);
     } catch (e) {
@@ -11,9 +12,9 @@ register("s-list", ({ items, render, item }) => {
   if (render) {
     return items.map(render).join("");
   }
-  else if (item) {
+  else if (element) {
     return items.map(it => {
-      return `${item.replace("{item}", it)}`;
+      return `${element.replace("{item}", it)}`;
     }).join("");
   }
 
